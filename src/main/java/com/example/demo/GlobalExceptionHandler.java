acf9dcad128b4a8d8e569c8c027045978df769e3
Xiran LYU <xiran.lyu@thoughtworks.com>
2020-10-21 18:23:03 +0800

    timestamp modified

--- src/main/java/com/example/demo/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/GlobalExceptionHandler.java
@@ -7,27 +7,34 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Date;
+import java.util.Locale;
 import java.util.Objects;
+import java.util.TimeZone;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(IdNotFoundException.class)
     public ResponseEntity<ErrorResult> handle(IdNotFoundException ex) {
-        String timeStamp = new Date().toString();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.sssZ", Locale.CHINA);
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
         Integer status = HttpStatus.NOT_FOUND.value();
         String error = HttpStatus.NOT_FOUND.getReasonPhrase();
-        ErrorResult errorResult = new ErrorResult(timeStamp, status, error, ex.getMessage());
+        ErrorResult errorResult = new ErrorResult(sdf.format(new Date()), status, error, ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResult);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResult> handle(MethodArgumentNotValidException ex) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.sssZ", Locale.CHINA);
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
         String message = Objects.requireNonNull(ex.getBindingResult().getFieldError()).getDefaultMessage();
         String timeStamp = new Date().toString();
         Integer status = HttpStatus.BAD_REQUEST.value();
         String error = HttpStatus.BAD_REQUEST.getReasonPhrase();
-        ErrorResult errorResult = new ErrorResult(timeStamp, status, error, message);
+        ErrorResult errorResult = new ErrorResult(sdf.format(new Date()), status, error, message);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResult);
     }
 }
